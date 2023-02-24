fd529e5d7d37f6eec76d5cc73008b23e7b024b42
mohammedshaneeb-ai <120699738+mohammedshaneeb-ai@users.noreply.github.com>
2023-02-24 23:19:46 +0530

    I am doing this only for studying GIT

---
+++ EDA_of_Titanic.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler,LabelEncoder
+# I am doing this only for studying GIT 
+# Don't main this
